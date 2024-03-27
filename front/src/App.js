edca55e3346f125dadd356af25b0498b8250c239
selimketata <121767998+selimketata@users.noreply.github.com>
2024-03-27 01:41:27 +0100

    Initial commit: Setup project structure and configuration

---
+++ front/src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Services from "./Pages/Services/Services";
+import News from "./Pages/News/News";
+import Contact from "./Pages/Contact/Contact";
+import Article from "./Pages/Article/Article";
+import Loader from "./Components/Loader/Loader";
+import "./App.css"
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/article" element={<Article />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/Loader" element={<Loader />} />
+        <Route path="*" element={<div>404 not found</div>} />
+      </Routes>
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/Pages/News/News.jsx
@@ -0,0 +1,9 @@
+import './News.css';
+
+function News() {
+  return (
+    <div>News</div>
+  );
+}
+
+export default News;
