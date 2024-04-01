2c4569bad1d2c703267bd0aeb78e0cfdc416dabe
syhamm830 <syrine.hammami@ensi-uma.tn>
2024-04-01 20:10:39 +0100

    changes

--- front/src/Pages/About/About.jsx
+++ front/src/Pages/About/About.jsx
@@ -1,18 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import './About.css'
-import rectangle1 from './assets/Rectangle 69.png'
-import rectangle2 from './assets/Rectangle 160.png'
-import rectangle3 from './assets/Rectangle 162.png'
-import pic1 from './assets/target 1.png'
-import pic2 from './assets/mission (1) 1.png'
+import rectangle1 from './assets/Rectangle1.png'
+import rectangle2 from './assets/rectangle2.png'
+import rectangle3 from './assets/rectangle3.png'
+import rectangle69 from './assets/Rectangle 69.png'
+import rectangle147 from './assets/Rectangle 147.png'
 import pic3 from './assets/passionne.png'
 import pic4 from './assets/meilleur-entrainement.png'
 import pic5 from './assets/innovation.png'
-import act1 from './assets/act1.png'
-import act2 from './assets/act2.png'
-import act3 from './assets/act3.png'
+
+
+
 function About(){
+    
+
     return(
         <>
+          
+          <div className='topp'>
+               <img src={rectangle69} />
+
+          </div>
+    
           <div className='recapropos'>
                <div className='bg1'>
                     <img src={rectangle1} alt="" />
@@ -25,7 +34,6 @@ function About(){
            <div className='vispic'>
                 <div className='recpic'>
                     <div className='rect2'><img src={rectangle2} alt="" /></div>
-                    <div className='pic1'><img src={pic1} alt="" style={{ backgroundColor: 'transparent' }} /></div>
                 </div>
                 <div className='text'>
                     <h1 style={{ fontFamily: 'roboto' }}>Notre vision</h1>
@@ -44,7 +52,6 @@ function About(){
                 </div>
                 <div className='recpic2'>
                     <div className='rect3'><img src={rectangle3} alt="" /></div>
-                    <div className='pic2'><img src={pic2} alt="" style={{ backgroundColor: 'transparent' }} /></div>
                 </div>
            </div>
            <br/><br/><br/><br/><br/>
@@ -53,18 +60,21 @@ function About(){
                 <div className='img1'>
                     <img src={pic3} alt="" />
                     <p>Passion</p>
+                    <h5 className='addedp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum dolor.</h5>
                 </div>
                 <div className='img2'>
                     <img src={pic4} alt="" />
                     <p>Excellence</p>
+                    <h5 className='addedp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum dolor.</h5>
                 </div>
                 <div className='img3'>
                     <img src={pic5} alt="" />
                     <p>Innovation</p>
+                    <h5 className='addedp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum dolor.</h5>
                 </div>
            </div>
            <br/><br/><br/><br/><br/>
-           <h2 style={{ fontFamily: 'roboto' }}>Nos activités</h2>
+           
         </>
     );
 }
