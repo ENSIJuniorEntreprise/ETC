8d42d8fa2d498cb2bb803c0e145b232837f37400
syhamm830 <syrine.hammami@ensi-uma.tn>
2024-04-05 00:17:43 +0100

    finalcss

--- front/src/Pages/About/About.jsx
+++ front/src/Pages/About/About.jsx
@@ -81,17 +81,20 @@ function About(){
                 <div className='img1'>
                     <img src={pic3} alt="" />
                     <p>Passion</p>
-                    <h5 className='addedp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum dolor.</h5>
+                    <h5 className='titlev'>Passion</h5>
+                    <h6 className='addedp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum dolor.</h6>
                 </div>
                 <div className='img2'>
                     <img src={pic4} alt="" />
                     <p>Excellence</p>
-                    <h5 className='addedp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum dolor.</h5>
+                    <h5 className='titlev'>Excellence</h5>
+                    <h6 className='addedp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum dolor.</h6>
                 </div>
                 <div className='img3'>
                     <img src={pic5} alt="" />
                     <p>Innovation</p>
-                    <h5 className='addedp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum dolor.</h5>
+                    <h5 className='titlev'>Innovation</h5>
+                    <h6 className='addedp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor lorem ipsum dolor.</h6>
                 </div>
            </div>
            <br/><br/><br/><br/><br/>
