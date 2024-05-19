9505efe840d6e16f9b837d22f721cd99715da7d8
selimketata <121767998+selimketata@users.noreply.github.com>
2024-05-19 05:53:41 +0100

    test

--- front/src/Pages/Services/Services.jsx
+++ front/src/Pages/Services/Services.jsx
@@ -1,6 +1,6 @@
 import './Services.css'
-import Section1 from '../components/section1/section1.jsx'
-import Section2 from '../components/section2/section2.jsx'
+import Section2 from './components/section2/section2.jsx'
+import Section1 from './components/section1/section1.jsx'
 export default function Services(){
     return(
         <div id='serv'>
